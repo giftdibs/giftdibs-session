64e96a02eddfb481b0f554b8d2fd9cf180310c0f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-20 14:41:27 -0400

    Fixed public API

--- projects/session/src/public_api.ts
+++ projects/session/src/public_api.ts
@@ -2,4 +2,8 @@
  * Public API Surface of session
  */
 
-export * from './lib/session';
+export * from './lib/session/authorization-providers';
+export * from './lib/session/authorization.interceptor';
+export * from './lib/session/session-user';
+export * from './lib/session/session.module';
+export * from './lib/session/session.service';
