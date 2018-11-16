b4e978ce7374e2599a4864c15aeba2097a99d1ae
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-11-16 13:03:17 -0500

    Updated session user interface

--- projects/session/src/lib/session/session-user.ts
+++ projects/session/src/lib/session/session-user.ts
@@ -1,7 +1,13 @@
 export interface SessionUser {
+  emailAddress?: string;
   emailAddressVerified?: boolean;
   facebookId?: string;
   firstName?: string;
   id?: string;
   lastName?: string;
+  notificationSettings?: {
+    [settingName: string]: {
+      allowEmail?: boolean;
+    }
+  };
 }
