6b574ea099cacfb90a2e9ce438cc9c99d2ceeb05
Steve Brush <stevebrush@aol.com>
2021-06-18 13:42:34 -0400

    Update to Angular 12

--- projects/session/src/lib/session/session.module.ts
+++ projects/session/src/lib/session/session.module.ts
@@ -1,46 +1,29 @@
-import {
-  NgModule,
-  ModuleWithProviders
-} from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 
-import {
-  CommonModule
-} from '@angular/common';
+import { CommonModule } from '@angular/common';
 
-import {
-  SessionService
-} from './session.service';
+import { SessionService } from './session.service';
 
-import {
-  SessionStartupModule
-} from './session-startup.module';
+import { SessionStartupModule } from './session-startup.module';
 
-import {
-  GD_API_URL
-} from './api-url-token';
+import { GD_API_URL } from './api-url-token';
 
-import {
-  GD_AUTHORIZATION_PROVIDERS
-} from '../authorization/authorization-providers';
+import { GD_AUTHORIZATION_PROVIDERS } from '../authorization/authorization-providers';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SessionStartupModule
-  ],
-  providers: [
-    GD_AUTHORIZATION_PROVIDERS,
-    SessionService
-  ]
+  imports: [CommonModule, SessionStartupModule],
+  providers: [GD_AUTHORIZATION_PROVIDERS, SessionService],
 })
 export class SessionModule {
   public static forRoot(apiUrl: string): ModuleWithProviders<SessionModule> {
     return {
       ngModule: SessionModule,
-      providers: [{
-        provide: GD_API_URL,
-        useValue: apiUrl
-      }]
+      providers: [
+        {
+          provide: GD_API_URL,
+          useValue: apiUrl,
+        },
+      ],
     };
   }
 }
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  apiUrl: 'https://giftdibs-api.herokuapp.com/v1',
-  production: true
-};
